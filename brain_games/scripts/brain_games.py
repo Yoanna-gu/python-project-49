2c9131952da1afe6b6044c416a77caa48e395dec
Yana Gubskaya <Yana.gubskaja87@gmail.com>
2024-03-29 21:56:24 +0300

    Third step: make welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+from cli import welcome_user
+
+
 def greet():
     print("Welcome to the Brain Games!")
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
