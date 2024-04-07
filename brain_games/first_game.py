3d2097829116c749c2090f9f1773cb71f6736d19
Yana Gubskaya <Yana.gubskaja87@gmail.com>
2024-04-07 20:04:48 +0300

    Make firts game: is even

---
+++ brain_games/first_game.py
@@ -0,0 +1,44 @@
+import prompt
+from random import randint
+
+
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def is_correct(answer, number):
+    if is_even(number) is True and answer == 'yes':
+        return 'correct'
+    elif is_even(number) is False and answer == 'no':
+        return 'correct'
+    else:
+        return 'wrong'
+
+
+def get_correct_answer(number):
+    if is_even(number) is True:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def is_even_game():
+    name = prompt.string("May I have your name?")
+    print(f"Hello, {name}!")
+    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    correct_answer_count = 0
+
+    while correct_answer_count < 3:
+        number = randint(0, 100)
+        answer = prompt.string(f'Question: {number}')
+        if is_correct(answer, number) == 'wrong':
+            correct_answer = get_correct_answer(number)
+            print(f'"{answer}" is wrong answer;( . Correct amswer was "{correct_answer}". \n Let\'s try again, {name}!')
+            break
+        print('Correct!')
+        correct_answer_count += 1
+    else:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from ..first_game import is_even_game
+
+
+def greet():
+    print("Welcome to the Brain Games!")
+
+
+def main():
+    greet()
+    is_even_game()
+
+
+if __name__ == '__main__':
+    main()
